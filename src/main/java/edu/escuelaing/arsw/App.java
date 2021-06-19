899dc6237781ed4c3a47f938e084294128ac9ee7
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-06-19 12:53:13 -0500

    Project structure

---
+++ src/main/java/edu/escuelaing/arsw/App.java
@@ -0,0 +1,17 @@
+package edu.escuelaing.arsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Clase principal del programa
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
